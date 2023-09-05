3271600a17d8fb399cd357c6afb96171b859dbba
@nil$$ <anilkj8@gmail.com>
2023-09-05 15:56:12 +0530

    sidemenu

---
+++ src/app/sidemenu/sidemenu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.css']
+})
+export class SidemenuComponent {
+
+menuname: Array<string>=['Home','About Us','Employee Management'];
+
+
+}
